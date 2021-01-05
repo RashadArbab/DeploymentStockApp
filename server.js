f5aa58a47294efbe1cd3248401ec1d3ce7ec6292
RashadArbab <rashad.arbab@gmail.com>
2021-01-05 09:57:17 -0500

    reverted to make it better client/package.json and package.json and server.js

--- server.js
+++ server.js
@@ -33,9 +33,11 @@ app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    /*
     app.get("*", (req , res)=>{
         res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'))
     })
+    */
 }
 
 
